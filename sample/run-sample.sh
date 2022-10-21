a704f21e2d067f371aab8aa081b44ac7257a9580
Jose Macchi <jose.macchi@gmail.com>
2022-10-20 22:11:36 -0300

    Sample code

--- sample/run-sample.sh
+++ sample/run-sample.sh
@@ -2,6 +2,6 @@
 # create temp folder (if does not exists)
 mkdir -p ./data/nomads_ncep_noaa
 # get last forecast and historical info
-docker run -v $(pwd)/data/nomads_ncep_noaa:/tmp/datastore -v $(pwd)/sample-config.sh:/scripts/config.sh -i -t --name ncep-weather-worker geotekne/ncep-weather-worker:1.0.0 bash
+docker run -v $(pwd)/data/nomads_ncep_noaa:/tmp/datastore -v $(pwd)/config.sh:/scripts/config.sh -i -t --name ncep-weather-worker geotekne/ncep-weather-worker:1.0.0 bash
 # remove docker container
 docker rm ncep-weather-worker
